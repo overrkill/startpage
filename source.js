c4f9861eda9c28739a0df6a5fadb8838b8d648bb
Abhishek Kale <zabhishekkale@gmail.com>
2020-07-23 23:15:54 +0530

    added clock

--- source.js
+++ source.js
@@ -2,3 +2,10 @@ const search = document.getElementsByClassName("search-txt");
 console.log(search[0]);
 search[0].focus();
 search[0].select()
+
+const clock = document.getElementById("clock")
+
+setInterval(() => {
+    const dt = new Date();
+    clock.innerHTML = `${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`
+}, 100)
